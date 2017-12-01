347b6df5a9592d35dbdea3b87ae85a5e5db5ea79
Mindmie <dreamiing.allday@gmail.com>
2017-12-02 02:33:53 +0700

    Initial commit

---
+++ app/src/main/java/com/example/mindmie/androiddemo/Question1Activity.java
@@ -0,0 +1,56 @@
+package com.example.mindmie.androiddemo;
+
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.nfc.Tag;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.util.Calendar;
+
+public class Question1Activity extends AppCompatActivity {
+
+    private static final String TAG = "Question1Activity";
+
+    private TextView displayDate;
+    private DatePickerDialog.OnDateSetListener mDateSetListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question1);
+        displayDate = findViewById(R.id.et_date);
+
+        displayDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog= new DatePickerDialog(
+                        Question1Activity.this,
+                        android.R.style.Theme_Holo_Dialog_MinWidth,
+                        mDateSetListener,
+                        year,month,day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+            }
+        });
+        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month = month + 1;
+                Log.d(TAG, "onDateSet: dd/mm/yyyy" + day + "/" + month + "/" + year);
+                String date = day + "/" + month + "/" + year;
+                displayDate.setText(date);
+            }
+        };
+    }
+}
